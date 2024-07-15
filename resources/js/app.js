c4896c5173d0e1ad77a9f14cfc8f659dc2ff3dba
Riccardo Petricca <3359943+rc2pc2@users.noreply.github.com>
2024-07-15 12:34:50 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+// importiamo il nostro scss generale
+import '~resources/scss/app.scss';
+
+// Prende il JS di bootstrap
+import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+    '../img/**'
+])
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('page-title', '')
+    </title>
+    {{-- generalcss --}}
+    @yield('head-cdn')
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    @include('partials.footer')
+
+    @yield('custom-scripts')
+</body>
+</html>
+{{-- ! chiunque estendera' questo layout potra' inserire al posto di ogni yield('nome-posto') quello che vuole attraverso una chiamata section('nome-posto') --}}
